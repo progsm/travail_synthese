d64fec5dd7fa4d3b65311c38e506ad560cd91af9
progsm <smillette79@hotmail.com>
2021-12-11 16:07:22 -0500

    intégrationConpleté

--- js/components/lecteur.js
+++ js/components/lecteur.js
@@ -4,14 +4,16 @@ export default externalTemplate({
 
     template_url: "js/components/lecteur.html",
 
-    // props: ['chanson'],
+    props: ['musique'],
+            
 
     // data
-    data(){
-        return{
-            
-            musiques:[],
+    data() {
+        return {
+            musiques: [],
             identité: 0,
+            image:"",
+            tag:"",
         }
     },
 
@@ -21,25 +23,26 @@ export default externalTemplate({
         fetch("chansons.json").then(resp => {
             resp.json().then(contenu_json => {
                 this.musiques = contenu_json
-                console.log(this.musiques)
             })
         })
 
-        
-       
+
+
     },
 
 
     // methodes
-    methods:{
+    methods: {
 
         // methode pour filtrer les jeux dans le tableau
         // filtrerMusique(une_chanson) {
-        //     if(this.identité != 0){
-        //         if(this.identité > une_chanson.id)
-        //         return true
+        //     
         //     }
         // },   
+    },
+    afficherTags(){
+        
+        this.tag = "musique.tags"
     },
 
 })
