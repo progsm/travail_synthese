d67acdd5da0a05123c2bcd6e3f76d8e6beec0bda
progsm <smillette79@hotmail.com>
2021-12-17 13:35:06 -0500

    play pause fonctionne

--- js/components/lecteur.js
+++ js/components/lecteur.js
@@ -10,8 +10,10 @@ export default externalTemplate({
         return {
             audio: new Audio(),
             musiques: [],
-            chanson: "",
-            image: "../images/fond_image.jpg",
+            chanson: {
+                image: "fond_image.jpg"
+            },
+            // image: "fond_image.jpg",
             temps: "0",
             avancement_temps: "0",
             tags: [],
@@ -21,7 +23,7 @@ export default externalTemplate({
                 backgroundColor: "",
             },
             recherche: "",
-            // chanson_active: false
+         
 
         }
     },
@@ -35,6 +37,19 @@ export default externalTemplate({
             })
         })
 
+        this.audio.addEventListener("play", e => {
+            this.play = true
+        })
+
+        this.audio.addEventListener("pause", e => {
+            this.play = false
+        })
+        // qund le temps avance
+        this.audio.addEventListener("timeupdate", e => {
+            // mon temps actuel = currentTime =>represente a combien je suis rendu 
+            this.avancement_temps = Math.round(this.audio.currentTime)
+        })
+
     },
 
 
@@ -43,35 +58,30 @@ export default externalTemplate({
 
         // methode pour filtrer les chansons dans le tableau
         filtrerMusique(une_chanson) {
+            // si this.recherche != ""
+
+            // si une_chanson.titre != this.recherche
+            //   return false
+
+            // si une_chanson.tags contiens this.recherche == false
+            //   return false
 
             return true
         },
 
         retourneChanson(choix) {
-            this.image = choix.image
+            // this.image = choix.image
             this.temps = choix.temps
             this.tags = choix.tags
             this.chanson = choix
 
-            this.audio.addEventListener("play", e => {
-                this.play = true
-            })
 
-            this.audio.addEventListener("pause", e => {
-                this.play = false
-            })
-            // qund le temps avance
-            this.audio.addEventListener("timeupdate", e => {
-                // mon temps actuel = currentTime =>represente a combien je suis rendu 
-                this.avancement_temps = this.audio.currentTime
-            })
-            
+
             // <audio src="food-vlog-11204.mp3">
             this.audio.setAttribute("src", 'audio/' + this.chanson.audio)
-            this.audio.play() 
+            this.audio.play()
             this.audio.volume = 0.1
-            // this.audio.pause()
-            // console.log(this.audio)
+            
 
 
         },
@@ -79,26 +89,23 @@ export default externalTemplate({
         changerSeconde(nombre) {
             let minutes = Math.floor(nombre / 60);
             let seconds = ((nombre % 60));
+
+            // si seconds est plus petit que 10
+            //     ajouter un zéro au début
+
             return minutes + ":" + (seconds);
         },
 
         togglePlay() {
 
-            // play, pause, ended, timeupdate
-            
+           
 
-            // if (this.play) {
-            //     this.audio.pause();
-            // } else {
-            //     this.audio.play();
-            // }
-
-            if(this.play = true){
+            if (this.play == true) {
                 this.audio.pause()
             }
-            
-            if(this.play = false){
-                this.audio.pause()
+
+            if (this.play == false) {
+                this.audio.play()
             }
 
         },
